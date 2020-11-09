da9e0fba78bc11dcfe6ff514e4070ad358539b6d
Thomas Schmid <tschm300@gmail.com>
2020-11-09 14:14:04 +0100

    CSharp-basics

---
+++ Assets/Scripts/ExampleClass.cs
@@ -0,0 +1,27 @@
+﻿//Bibliotheken / Asembly Referenz / Namespaces
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Deklaration einer Klasse
+
+// public 		-> access modifier (Zugriffsrechte) | keyword (vorreservierte Schlüsselwörter)
+// class		-> keyword zur Deklaration einer Klasse
+// ExampleClass		-> identifier (Name der Klasse)
+// : MonoBehaviour	-> Erstellen einer Unterklasse
+// {}			-> Scope/Wirkungsbereich
+
+public class ExampleClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
