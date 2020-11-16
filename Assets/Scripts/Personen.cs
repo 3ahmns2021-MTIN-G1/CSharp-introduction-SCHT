1cb6848ccb1f2538ed1e8470b6be4de1c764eed6
Thomas Schmid <tschm300@gmail.com>
2020-11-16 14:16:20 +0100

    last update 16.11

---
+++ Assets/Scripts/Personen.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personen : MonoBehaviour
+{
+    public string name;     // Name
+    public int alter;       // Alter
+    public Personen vater;  // Vater
+    public Personen vater;  // Mutter
+}
