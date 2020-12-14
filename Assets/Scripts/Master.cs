a206539e70384a2b4c5c888fe13df2ebc7173ec6
Thomas Schmid <tschm300@gmail.com>
2020-12-14 11:36:15 +0100

    Update 14-12-2020

--- Assets/Scripts/Master.cs
+++ Assets/Scripts/Master.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
 
 public class Master : MonoBehaviour
 {
     public float x;
     public bool z;
+    public int textSize;
     public Rigidbody rigid;
     public BoxCollider bc;
-
+    public Text txt;
+   
     void Start()
     {
         rigid = GetComponent<Rigidbody>();
@@ -17,5 +21,8 @@ public class Master : MonoBehaviour
         rigid.angularDrag = x + 50f;
         rigid.drag = x - rigid.mass;
         bc.isTrigger = z;
+        txt.text = "Wilfried Gruber";
+        txt.fontSize = 4;
+        txt.lineSpacing = 5;
     }
 }
---
+++ Assets/Scripts/Wiederholung.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.Video;
+
+public class Wiederholung : MonoBehaviour
+{
+    public bool loop;
+    public Tilemap tm;
+    public VideoPlayer vp;
+
+    void Start()
+    {
+        tm.animationFrameRate = 5f;
+        vp.isLooping = loop;
+    }
+
+ 
+    void Update()
+    {
+        
+    }
+}
